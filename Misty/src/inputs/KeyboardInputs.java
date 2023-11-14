b15b3c86d32c1d50fdb1e9504b5a266c1d9e73c0
Ditto Tiu <89990545+dittttt@users.noreply.github.com>
2023-11-14 14:48:25 +0800

    Updated Character Animations

--- Misty/src/inputs/KeyboardInputs.java
+++ Misty/src/inputs/KeyboardInputs.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import main.GamePanel;
-
+import static utilz.Constants.Directions.*;
 public class KeyboardInputs implements KeyListener{
 	
 	private GamePanel gamePanel;
@@ -19,26 +19,32 @@ public class KeyboardInputs implements KeyListener{
 	}
 	
 	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void keyReleased(KeyEvent e) {
+		switch(e.getKeyCode()) {
+		case KeyEvent.VK_W:
+		case KeyEvent.VK_A:
+		case KeyEvent.VK_S:
+		case KeyEvent.VK_D:
+			gamePanel.setMoving(false);
+			break;
+		}
 	}
 	
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyPressed(KeyEvent e) {
 		
 		switch(e.getKeyCode()) {
 		case KeyEvent.VK_W:
-			gamePanel.changeYDelta(-5);
+			gamePanel.setDirection(UP);
 			break;
 		case KeyEvent.VK_A:
-			gamePanel.changeXDelta(-5);
+			gamePanel.setDirection(LEFT);
 			break;
 		case KeyEvent.VK_S:
-			gamePanel.changeYDelta(5);
+			gamePanel.setDirection(DOWN);
 			break;
 		case KeyEvent.VK_D:
-			gamePanel.changeXDelta(5);
+			gamePanel.setDirection(RIGHT);
 			break;
 		}
 		
