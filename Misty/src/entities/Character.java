b40a72e72d78df1da431e3dedd7e2981afef7d1e
dittttt <dittotiu111@yahoo.com>
2025-06-14 05:13:58 +0800

    Created new character class that stores otherwise redundant code from Player and Enemy classes. Also added a health bar system. Working on attacking as a player now

---
+++ Misty/src/entities/Character.java
@@ -0,0 +1,415 @@
+package entities;
+
+import static utilz.Constants.PlayerConstants.DEATH;
+import static utilz.Constants.PlayerConstants.FALLING;
+import static utilz.Constants.PlayerConstants.GetSpriteAmount;
+import static utilz.Constants.PlayerConstants.HEAVY_ATTACK;
+import static utilz.Constants.PlayerConstants.HIT;
+import static utilz.Constants.PlayerConstants.IDLE;
+import static utilz.Constants.PlayerConstants.JUMP;
+import static utilz.Constants.PlayerConstants.LIGHT_ATTACK;
+import static utilz.Constants.PlayerConstants.ROLL;
+import static utilz.Constants.PlayerConstants.RUNNING;
+import static utilz.Constants.PlayerConstants.WALKING;
+import static utilz.HelpMethods.CanMoveHere;
+import static utilz.HelpMethods.GetEntityXPosNextToWall;
+import static utilz.HelpMethods.GetEntityYPosUnderRoofOrAboveFloor;
+import static utilz.HelpMethods.IsEntityOnFloor;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import main.Game;
+
+public abstract class Character extends Entity {
+    // Common fields
+    protected BufferedImage[][] animations;
+    protected int aniTick, aniIndex, aniSpeed = 25;
+    protected int characterAction = IDLE;
+    protected boolean moving = false;
+    protected boolean light_attack = false;
+    protected boolean heavy_attack = false;
+    protected boolean roll = false;
+    protected boolean rollingForward = false;
+    protected boolean canMove = true;
+    protected boolean normalMirrorState = true;
+    protected int rollDuration = 1;
+    protected int rollCounter = 0;
+    protected boolean continueMovingAfterRoll = false;
+    protected boolean left, right, jump, run, death, hit;
+    protected float characterSpeed = 0.6f * Game.SCALE;
+    protected int[][] lvlData;
+    protected int xDrawOffsetValue = 44;
+    protected float xDrawOffset = xDrawOffsetValue * Game.SCALE;
+    protected float yDrawOffset = 36 * Game.SCALE;
+    protected float airSpeed = 0f;
+    protected float gravity = 0.04f * Game.SCALE;
+    protected float jumpSpeed = -2.25f * Game.SCALE;
+    protected float fallSpeedAfterCollision = 1.0f * Game.SCALE;
+    protected boolean inAir = false;
+    protected int health = 100;
+    protected Color healthBarFillColor;
+    protected Color healthBarBgColor;
+    
+    public Character(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        initHitbox(x, y, (int) (20 * Game.SCALE), (int) (30 * Game.SCALE));
+    }
+    
+    // Common methods
+    protected void updateCharacter() {
+        updatePos();
+        updateAnimationTick();
+        setAnimation();
+        updateXDrawOffset();
+    }
+    
+    protected void updateXDrawOffset() {
+        if (left) {
+            xDrawOffsetValue = 48;
+        } else if (right) {
+            xDrawOffsetValue = 44;
+        }
+        xDrawOffset = xDrawOffsetValue * Game.SCALE;
+    }
+    
+    public void renderCharacter(Graphics g, BufferedImage[][] animations) {
+        BufferedImage currentFrame = animations[characterAction][aniIndex];
+        
+        if (normalMirrorState) {
+            g.drawImage(currentFrame, (int) (hitbox.x - xDrawOffset), (int) (hitbox.y - yDrawOffset), width, height, null);
+        } else {
+            g.drawImage(currentFrame, (int) (hitbox.x - xDrawOffset + width), (int) (hitbox.y - yDrawOffset), -width, height, null);
+        }
+    }
+    
+    protected void renderHealthBar(Graphics g) {
+        // Health bar dimensions
+        int healthBarWidth = 85;
+        int healthBarHeight = 8;
+        int healthBarOffset = -145;
+        
+        // Calculate position relative to character
+        int x = (int) (hitbox.x - xDrawOffset + width / 2 - healthBarWidth / 2);
+        int y = (int) (hitbox.y - yDrawOffset - healthBarHeight - healthBarOffset);
+        int currentHealthWidth = (int) ((health / 100.0) * healthBarWidth);
+
+        // Draw health bar (subclasses will set colors)
+        g.setColor(healthBarBgColor);
+        g.fillRect(x, y, healthBarWidth, healthBarHeight);
+        
+        g.setColor(healthBarFillColor);
+        g.fillRect(x, y, currentHealthWidth, healthBarHeight);
+        
+        // Draw border
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, healthBarWidth, healthBarHeight);
+        
+        // Draw health text
+        g.setColor(Color.WHITE);
+        g.drawString(health + "/100", x + healthBarWidth / 2 - 15, y - 2);
+    }
+    
+    protected void updateAnimationTick() {
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= GetSpriteAmount(characterAction)) {
+                aniIndex = 0;
+                light_attack = false;
+                heavy_attack = false;
+                roll = false;
+                death = false;
+                hit = false;
+            }
+        }
+    }
+    
+    protected void setAnimation() {
+        int startAni = characterAction;
+        
+        if (inAir) {
+            if (airSpeed < 0)
+                characterAction = JUMP;
+            else
+                characterAction = FALLING;
+        } else {
+            if (moving) {
+                if (run && (left || right)) {
+                    characterAction = RUNNING;
+                    normalMirrorState = right;
+                } else if (run && (!left || !right)) {
+                    characterAction = IDLE;
+                } else if (left || right) {
+                    characterAction = WALKING;
+                    normalMirrorState = right;
+                }
+            } else {
+                characterAction = IDLE;
+            }
+            
+            if (light_attack)
+                characterAction = LIGHT_ATTACK;
+            if (heavy_attack)
+                characterAction = HEAVY_ATTACK;
+            if (hit)
+                characterAction = HIT;
+            if (death)
+                characterAction = DEATH;
+            
+            handleRollAnimation();
+        }
+        
+        if (startAni != characterAction) {
+            resetAniTick();
+        }
+    }
+    
+    private void handleRollAnimation() {
+        if (roll) {
+            characterAction = ROLL;
+            if (!rollingForward) {
+                rollingForward = true;
+                rollCounter = 0;
+                canMove = false;
+            }
+            
+            if (rollingForward && rollCounter < rollDuration) {
+                float xSpeed = characterSpeed * 2.0f * (normalMirrorState ? 1 : -1);
+                updateXPos(xSpeed);
+                rollCounter++;
+            } else {
+                rollingForward = false;
+                rollCounter = 0;
+                if (right || left) {
+                    float xSpeed = characterSpeed * (right ? 1 : -1);
+                    updateXPos(xSpeed);
+                }
+                canMove = true;
+            }
+        }
+    }
+    
+    protected void resetAniTick() {
+        aniTick = 0;
+        aniIndex = 0;
+    }
+    
+    protected void updatePos() {
+        moving = false;
+        
+        if (!inAir) {
+            if (roll) {
+                characterSpeed = 0.75f * Game.SCALE;
+                canMove = false;
+                aniSpeed = 20;
+                continueMovingAfterRoll = left || right;
+            } else {
+                canMove = true;
+            }
+        }
+        
+        if (canMove) {
+            handleMovementSpeed();
+            handleSpecialAttacks();
+            
+            if (jump) {
+                jump();
+            }
+            
+            if (!(left || right || inAir || run)) {
+                return;
+            }
+            
+            float xSpeed = calculateXSpeed();
+            handleAirMovement(xSpeed);
+        }
+    }
+    
+    private void handleMovementSpeed() {
+        if (!run) {
+            aniSpeed = 30;
+            if (left || right) {
+                characterSpeed = 0.6f * Game.SCALE;
+            }
+        } else if (!light_attack || !heavy_attack) {
+            if (run && (left || right)) {
+                aniSpeed = 20;
+                characterSpeed = 0.85f * Game.SCALE;
+            } else if (run && (!left || !right)) {
+                aniSpeed = 30;
+                characterSpeed = 0.6f * Game.SCALE;
+            }
+        }
+    }
+    
+    private void handleSpecialAttacks() {
+        if (light_attack) {
+            aniSpeed = 20;
+        }
+        if (heavy_attack) {
+            aniSpeed = 21;
+        }
+        if (hit) {
+            aniSpeed = 12;
+        }
+        if (death) {
+            aniSpeed = 12;
+        }
+    }
+    
+    private float calculateXSpeed() {
+        float xSpeed = 0;
+        if (left) {
+            xSpeed -= characterSpeed;
+        } else if (right) {
+            xSpeed += characterSpeed;
+        }
+        return xSpeed;
+    }
+    
+    private void handleAirMovement(float xSpeed) {
+        if (!inAir) {
+            if (!IsEntityOnFloor(hitbox, lvlData)) {
+                inAir = true;
+            }
+        }
+        
+        if (inAir) {
+            handleVerticalMovement(xSpeed);
+        } else {
+            updateXPos(xSpeed);
+            moving = true;
+            handlePostRollMovement();
+        }
+    }
+    
+    private void handleVerticalMovement(float xSpeed) {
+        if (CanMoveHere(hitbox.x, hitbox.y + airSpeed, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.y += airSpeed;
+            airSpeed += gravity;
+            updateXPos(xSpeed);
+        } else {
+            hitbox.y = GetEntityYPosUnderRoofOrAboveFloor(hitbox, airSpeed);
+            if (airSpeed > 0) {
+                resetInAir();
+            } else {
+                airSpeed = fallSpeedAfterCollision;
+            }
+            updateXPos(xSpeed);
+        }
+    }
+    
+    private void handlePostRollMovement() {
+        if (roll && !rollingForward && continueMovingAfterRoll) {
+            float additionalXSpeed = characterSpeed * (right ? 1 : -1);
+            updateXPos(additionalXSpeed);
+        }
+    }
+    
+    protected void jump() {
+        if (inAir)
+            return;
+        inAir = true;
+        airSpeed = jumpSpeed;
+    }
+    
+    protected void resetInAir() {
+        inAir = false;
+        airSpeed = 0;
+    }
+    
+    protected void updateXPos(float xSpeed) {
+        if (CanMoveHere(hitbox.x + xSpeed, hitbox.y, hitbox.width, hitbox.height, lvlData)) {
+            hitbox.x += xSpeed;
+        } else {
+            hitbox.x = GetEntityXPosNextToWall(hitbox, xSpeed);
+        }
+    }
+    
+    public void loadLvlData(int[][] lvlData) {
+        this.lvlData = lvlData;
+        if (!IsEntityOnFloor(hitbox, lvlData))
+            inAir = true;
+    }
+    
+    public void resetDirBooleans() {
+        left = false;
+        right = false;
+        run = false;
+    }
+    
+    // Getters and setters
+    public int getHealth() {
+        return health;
+    }
+    
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    public boolean isLeft() {
+        return left;
+    }
+    
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+    
+    public boolean isRight() {
+        return right;
+    }
+    
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+    
+    public void setJump(boolean jump) {
+        this.jump = jump;
+    }
+    
+    public boolean isRunning() {
+        return run;
+    }
+    
+    public void setRun(boolean run) {
+        this.run = run;
+    }
+    
+    public void setLightAttack(boolean light_attack) {
+        this.light_attack = light_attack;
+    }
+    
+    public void setHeavyAttack(boolean heavy_attack) {
+        this.heavy_attack = heavy_attack;
+    }
+    
+    public void toggleLightAttack(boolean light_attack) {
+        this.light_attack = true;
+    }
+    
+    public void toggleHeavyAttack(boolean heavy_attack) {
+        this.heavy_attack = true;
+    }
+    
+    public void toggleRoll(boolean roll) {
+        this.roll = true;
+    }
+    
+    public void toggleHit(boolean hit) {
+        this.hit = true;
+    }
+    
+    public void toggleDeath(boolean death) {
+        this.death = true;
+    }
+    
+    // Abstract methods for character-specific implementations
+    public abstract void update();
+    public abstract void render(Graphics g);
+    public abstract void loadAnimations();
+    public abstract float getCharacterX();
+    public abstract float getCharacterY();
+}
--- Misty/src/gamestates/Playing.java
+++ Misty/src/gamestates/Playing.java
@@ -1,5 +1,6 @@
 package gamestates;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -11,157 +12,210 @@ import main.Game;
 import ui.PauseOverlay;
 
 public class Playing extends State implements Statemethods {
-	private Player player;
-	private Enemy enemy;
-	private LevelManager levelManager;
-	private PauseOverlay pauseOverlay;
-	private boolean paused = false;
+    private Player player;
+    private Enemy enemy;
+    private LevelManager levelManager;
+    private PauseOverlay pauseOverlay;
+    private boolean paused = false;
 
-	public Playing(Game game) {
-		super(game);
-		initClasses();
-	}
+    // Health bar dimensions
+    private static final int HEALTH_BAR_WIDTH = 200;
+    private static final int HEALTH_BAR_HEIGHT = 20;
+    private static final int HEALTH_BAR_Y_OFFSET = 10;
+    private static final int HEALTH_BAR_X_PADDING = 20;
 
-	private void initClasses() {
-		levelManager = new LevelManager(game);
-		player = new Player(100, 200, (int) (112 * Game.SCALE), (int) (72 * Game.SCALE));
-		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
-		enemy = new Enemy(1719, 200, (int) (112 * Game.SCALE), (int) (72 * Game.SCALE));
-		enemy.loadLvlData(levelManager.getCurrentLevel().getLevelData());
-		pauseOverlay = new PauseOverlay(this);
-	}
+    // Health bar colors
+    private static final Color PLAYER_HEALTH_COLOR = new Color(0, 180, 0);  // Vibrant green
+    private static final Color PLAYER_BG_COLOR = new Color(0, 80, 0);       // Dark green
+    private static final Color ENEMY_HEALTH_COLOR = new Color(200, 0, 0);   // Deep red
+    private static final Color ENEMY_BG_COLOR = new Color(80, 0, 0);        // Dark red
+    private static final Color HEALTH_BAR_BORDER = Color.BLACK;
 
-	@Override
-	public void update() {
-		if (!paused) {
-			levelManager.update();
-			player.update();
-			float playerX = player.getPlayerX(); // Obtain player's X coordinate
-			float playerY = player.getPlayerY(); // Obtain player's Y coordinate
-			enemy.update(playerX, playerY);
-		} else {
-			pauseOverlay.update();
-		}
-	}
+    public Playing(Game game) {
+        super(game);
+        initClasses();
+    }
 
-	@Override
-	public void draw(Graphics g) {
-		levelManager.draw(g);
-		player.render(g);
-		enemy.render(g);
+    private void initClasses() {
+        levelManager = new LevelManager(game);
+        player = new Player(100, 200, (int) (112 * Game.SCALE), (int) (72 * Game.SCALE));
+        player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+        enemy = new Enemy(1719, 200, (int) (112 * Game.SCALE), (int) (72 * Game.SCALE), player);
+        enemy.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+        pauseOverlay = new PauseOverlay(this);
+    }
 
-		if (paused)
-			pauseOverlay.draw(g);
+    @Override
+    public void update() {
+        if (!paused) {
+            levelManager.update();
+            player.update();
+            enemy.update();
+        } else {
+            pauseOverlay.update();
+        }
+    }
 
-	}
+    @Override
+    public void draw(Graphics g) {
+        levelManager.draw(g);
+        player.render(g);
+        enemy.render(g);
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1)
-			player.setLightAttack(true);
+        // Draw health bars
+        drawPlayerHealthBar(g);
+        drawEnemyHealthBar(g);
 
-		else if (e.getButton() == MouseEvent.BUTTON3)
-			player.setHeavyAttack(true);
-	}
+        if (paused)
+            pauseOverlay.draw(g);
+    }
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_A:
-			player.setLeft(true);
-			break;
-		case KeyEvent.VK_D:
-			player.setRight(true);
-			break;
-		case KeyEvent.VK_SPACE:
-			player.setJump(true);
-			break;
-//IF GANAHAN TA MA CANCEL
-		case KeyEvent.VK_J:
-			player.toggleLightAttack(true);
-			break;
-		case KeyEvent.VK_K:
-			player.toggleHeavyAttack(true);
-			break;
-		case KeyEvent.VK_SHIFT:
-			player.setRun(true);
-			break;
-		case KeyEvent.VK_CONTROL:
-		case KeyEvent.VK_C:
-			player.toggleRoll(true);
-			break;
-		case KeyEvent.VK_H:
-			player.toggleHit(true);
-			break;
-		case KeyEvent.VK_ESCAPE:
-			paused = !paused;
-			break;
-		}
-	}
+    private void drawPlayerHealthBar(Graphics g) {
+        int x = HEALTH_BAR_X_PADDING;
+        int y = HEALTH_BAR_Y_OFFSET;
+        
+        // Draw background
+        g.setColor(PLAYER_BG_COLOR);
+        g.fillRect(x, y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
+        
+        // Draw health fill
+        int healthWidth = (int)((player.getHealth() / 100.0) * HEALTH_BAR_WIDTH);
+        g.setColor(PLAYER_HEALTH_COLOR);
+        g.fillRect(x, y, healthWidth, HEALTH_BAR_HEIGHT);
+        
+        // Draw border
+        g.setColor(HEALTH_BAR_BORDER);
+        g.drawRect(x, y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
+        
+        // Draw health text
+        g.setColor(Color.WHITE);
+        g.drawString("PLAYER: " + player.getHealth(), x + 5, y + HEALTH_BAR_HEIGHT - 5);
+    }
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_A:
-			player.setLeft(false);
-			break;
-		case KeyEvent.VK_D:
-			player.setRight(false);
-			break;
-		case KeyEvent.VK_SPACE:
-			player.setJump(false);
-			break;
-		case KeyEvent.VK_SHIFT:
-			player.setRun(false);
-			break;
-		}
+    private void drawEnemyHealthBar(Graphics g) {
+        // Use game's screen width directly
+        int screenWidth = Game.GAME_WIDTH;
+        int x = screenWidth - HEALTH_BAR_WIDTH - HEALTH_BAR_X_PADDING;
+        int y = HEALTH_BAR_Y_OFFSET;
+        
+        // Draw background
+        g.setColor(ENEMY_BG_COLOR);
+        g.fillRect(x, y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
+        
+        // Draw health fill
+        int healthWidth = (int)((enemy.getHealth() / 100.0) * HEALTH_BAR_WIDTH);
+        g.setColor(ENEMY_HEALTH_COLOR);
+        g.fillRect(x, y, healthWidth, HEALTH_BAR_HEIGHT);
+        
+        // Draw border
+        g.setColor(HEALTH_BAR_BORDER);
+        g.drawRect(x, y, HEALTH_BAR_WIDTH, HEALTH_BAR_HEIGHT);
+        
+        // Draw health text
+        g.setColor(Color.WHITE);
+        g.drawString("ENEMY: " + enemy.getHealth(), x + 5, y + HEALTH_BAR_HEIGHT - 5);
+    }
 
-	}
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1)
+            player.setLightAttack(true);
+        else if (e.getButton() == MouseEvent.BUTTON3)
+            player.setHeavyAttack(true);
+    }
 
-	public void mouseDragged(MouseEvent e) {
-		if (paused)
-			pauseOverlay.mouseDragged(e);
-	}
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_A:
+                player.setLeft(true);
+                break;
+            case KeyEvent.VK_D:
+                player.setRight(true);
+                break;
+            case KeyEvent.VK_SPACE:
+                player.setJump(true);
+                break;
+            case KeyEvent.VK_J:
+                player.toggleLightAttack(true);
+                break;
+            case KeyEvent.VK_K:
+                player.toggleHeavyAttack(true);
+                break;
+            case KeyEvent.VK_SHIFT:
+                player.setRun(true);
+                break;
+            case KeyEvent.VK_CONTROL:
+            case KeyEvent.VK_C:
+                player.toggleRoll(true);
+                break;
+            case KeyEvent.VK_H:
+                player.toggleHit(true);
+                break;
+            case KeyEvent.VK_ESCAPE:
+                paused = !paused;
+                break;
+        }
+    }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		if (paused)
-			pauseOverlay.mousePressed(e);
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_A:
+                player.setLeft(false);
+                break;
+            case KeyEvent.VK_D:
+                player.setRight(false);
+                break;
+            case KeyEvent.VK_SPACE:
+                player.setJump(false);
+                break;
+            case KeyEvent.VK_SHIFT:
+                player.setRun(false);
+                break;
+        }
+    }
 
-	}
+    public void mouseDragged(MouseEvent e) {
+        if (paused)
+            pauseOverlay.mouseDragged(e);
+    }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		if (paused)
-			pauseOverlay.mouseReleased(e);
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (paused)
+            pauseOverlay.mousePressed(e);
+    }
 
-	}
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if (paused)
+            pauseOverlay.mouseReleased(e);
+    }
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		if (paused)
-			pauseOverlay.mouseMoved(e);
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if (paused)
+            pauseOverlay.mouseMoved(e);
+    }
 
-	}
+    public void unpauseGame() {
+        paused = false;
+    }
 
-	public void unpauseGame() {
-		paused = false;
-	}
+    public void windowFocusLost() {
+        player.resetDirBooleans();
+    }
 
-	public void windowFocusLost() {
-		player.resetDirBooleans();
-	}
+    public Player getPlayer() {
+        return player;
+    }
 
-	public Player getPlayer() {
-		return player;
-	}
+    public Enemy getEnemy() {
+        return enemy;
+    }
 
-	public Enemy getEnemy() {
-		return enemy;
-	}
-
-	public PauseOverlay getPauseOverlay() {
-		return pauseOverlay; // Ensure `pauseOverlay` is properly initialized in the `Playing` class.
-	}
-
+    public PauseOverlay getPauseOverlay() {
+        return pauseOverlay;
+    }
 }
